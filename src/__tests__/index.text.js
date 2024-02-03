26254afb12ce011c38dd1dbe1f34dd3cd93295ac
mAmineChniti <emin.chniti@esprit.tn>
2024-02-03 15:51:45 +0100

    used require

--- src/__tests__/index.text.js
+++ src/__tests__/index.text.js
@@ -1,13 +1,17 @@
-import { cloneRepository, updatePackageName } from '../index';
+const { cloneRepository, updatePackageName } = require('../index');
 
 describe('cloneRepository', () => {
   test('clones the repository', async () => {
     // mock implementation
     const mockClone = jest.fn();
 
-    // import and mock git clone
-    jest.mock('git-clone', () => ({
+    // import and mock isomorphic-git clone
+    jest.mock('isomorphic-git', () => ({
       clone: mockClone,
+      http: {
+        node: jest.fn(),
+      },
+      fs: jest.fn(),
     }));
 
     const repoUrl = 'https://github.com/AminoffZ/catonaut.git';
@@ -15,39 +19,40 @@ describe('cloneRepository', () => {
 
     await cloneRepository(repoUrl, destinationPath);
 
-    expect(mockClone).toHaveBeenCalledWith(repoUrl, destinationPath, {
-      recursive: true,
+    expect(mockClone).toHaveBeenCalledWith({
+      fs: expect.any(Object),
+      http: expect.any(Object),
+      url: repoUrl,
+      dir: destinationPath,
     });
   });
 });
 
 describe('updatePackageName', () => {
-  test('updates the package name in package.json', () => {
+  test('updates the package name in package.json', async () => {
     const originalPackageJson = {
       name: 'original-name',
     };
 
-    const newPackageName = 'new-name';
     const packageJsonPath = '/tmp/new-app/package.json';
 
     // mock implementation
-    const mockWriteFile = jest.fn();
+    const mockReadJson = jest.fn(() => originalPackageJson);
+    const mockWriteJson = jest.fn();
 
-    jest.mock('fs', () => ({
-      promises: {
-        readFile: jest.fn(() => JSON.stringify(originalPackageJson)),
-        writeFile: mockWriteFile,
-      },
+    jest.mock('fs-extra', () => ({
+      ensureDir: jest.fn(),
+      readJson: mockReadJson,
+      writeJson: mockWriteJson,
     }));
 
-    return updatePackageName(packageJsonPath, newPackageName).then(() => {
-      expect(mockWriteFile).toHaveBeenCalledWith(
-        packageJsonPath,
-        JSON.stringify({
-          ...originalPackageJson,
-          name: newPackageName,
-        })
-      );
-    });
+    await updatePackageName(packageJsonPath);
+
+    expect(mockReadJson).toHaveBeenCalledWith(packageJsonPath);
+    expect(mockWriteJson).toHaveBeenCalledWith(
+      packageJsonPath,
+      expect.objectContaining({ name: path.basename('/tmp/new-app') }),
+      { spaces: 2 }
+    );
   });
 });
